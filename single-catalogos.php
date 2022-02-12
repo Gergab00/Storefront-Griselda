9dddf27ab4c69bdfb2f6c4e958ad530c209c7590
Gerardo Gonzalez <gergab00@hotmail.com>
2022-02-12 15:39:36 -0600

    Se agrego la plantilla para catalogos.

---
+++ single-catalogos.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Template Name:Página de Catalogo
+
+Template Post Type: post
+*/
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			do_action( 'storefront_single_post_before' );
+
+            the_title( '<h1 class="entry-title">', '</h1>' );
+
+			the_content();
+
+			do_action( 'storefront_single_post_after' );
+
+		endwhile; // End of the loop.
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+do_action( 'storefront_sidebar' );
+get_footer();
